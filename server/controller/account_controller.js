e2e0af689b02462c68ac8b12cbb2f6cfe619b931
Hussein Mubarak <husseinmubarak500@gmail.com>
2023-09-11 12:20:24 +0100

    ...

--- server/controller/account_controller.js
+++ server/controller/account_controller.js
@@ -1,9 +1,10 @@
 import Routes from "../routes/index_routes.js";
 import AccountService from "../services/account_service.js";
+import Upload from "../utils/uploads.js";
 
 class AccountController {
     static initialize(app) {
-        app.get(Routes.REGISTER, AccountService.register);
+        app.post(Routes.REGISTER, Upload.imageUpload().single('image'), AccountService.register);
     }
 }
 
