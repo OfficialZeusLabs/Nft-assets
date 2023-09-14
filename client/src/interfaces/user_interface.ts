db5b239e0caffe686250f0eae893c3d425221603
Hussein Mubarak <husseinmubarak500@gmail.com>
2023-09-14 10:08:25 +0100

    Redux: state management implemented

---
+++ client/src/interfaces/user_interface.ts
@@ -0,0 +1,10 @@
+export interface UserInterface {
+  title: string | null;
+  description: string | null;
+  whitepaper: string | null;
+  goal: string | null;
+  discordLink: string | null;
+  website: string | null;
+  email: string | null;
+  discordId: string | null;
+};
