f37a11991a2175b6ecc8db1df122954be40f092f
Hussein Mubarak <husseinmubarak500@gmail.com>
2023-09-18 15:32:48 +0100

    File services created

--- server/services/file_service.js
+++ server/services/file_service.js
@@ -1,8 +1,11 @@
 import { v2 as cloudinary } from 'cloudinary'
 import fs from 'fs';
+import ResponseHandler from '../utils/response_handler.js';
+import { StatusCodes } from 'http-status-codes';
 
 class FileService {
-    static async uploadToCloudinary(imageFile) {
+    static async uploadToCloudinary(req, res) {
+        const files = req.files;
         cloudinary.config({
             cloud_name: process.env.CLOUDINARY_NAME,
             api_key: process.env.CLOUDINARY_API_KEY,
@@ -10,9 +13,14 @@ class FileService {
             secure: true
         })
         try {
-            let uploadImage = await cloudinary.uploader.upload(imageFile);
-            let uploadedImage = await uploadImage.secure_url;
-            return uploadedImage;
+            let images = [];
+            for (let file of files) {
+                let uploadImage = await cloudinary.uploader.upload(file.path);
+                let uploadedImage = await uploadImage.secure_url;
+                images.push(uploadedImage);
+                await FileService.unlinkFileSync(file.path);
+            }
+            return ResponseHandler.sendResponseWithData(res, StatusCodes.OK, 'Artworks', images);
         }
         catch (error) {
             console.error(error);
