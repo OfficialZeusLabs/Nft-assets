7730ef03f08fe2ce16a23d14b2e70b179966ed20
Destiny Aigbe <aigbedestinyic@gmail.com>
2023-09-18 03:09:23 +0100

    feat: refactored contract and deployed on gnosis

--- core/contracts/Factory.sol
+++ core/contracts/Factory.sol
@@ -4,24 +4,24 @@ pragma solidity ^0.8.0;
 import {SimpleCollectible} from "./SimpleCollectible.sol";
 
 contract Factory {
-	address[] public marketplace;
+    address[] public marketplace;
 
-	event Deploy(address addr, string Name);
+    event Deploy(address addr, string Name);
 
-	function deploy(
-		string memory Name,
-		string memory Symbol,
-		string[] memory _URIs,
-		uint[] memory _mintFee
-	) external {
-		SimpleCollectible _contract = new SimpleCollectible(
-			Name,
-			Symbol,
-			_URIs,
-			_mintFee,
-			msg.sender
-		);
-		marketplace.push(address(_contract));
-		emit Deploy(address(_contract), Name);
-	}
+    function deploy(
+        string memory Name,
+        string memory Symbol,
+        string[] memory _URIs,
+        uint[] memory _mintFee
+    ) external {
+        SimpleCollectible _contract = new SimpleCollectible(
+            Name,
+            Symbol,
+            _URIs,
+            _mintFee,
+            msg.sender
+        );
+        marketplace.push(address(_contract));
+        emit Deploy(address(_contract), Name);
+    }
 }
