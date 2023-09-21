87ab9883c7f4f1c665439314605d0d4b08900924
EnebeliEmmanuel <iemmaenebeli@gmail.com>
2023-09-21 12:16:48 +0100

    [CHORE]-responsive

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,12 +1,49 @@
 "use client";
 import { Provider } from "react-redux";
+import Providers from "@/providers/walletconnect";
 import store from "@/store";
-import { poppins } from "@/fonts/fonts";
+import { poppins } from "../fonts/fonts";
+import type { Metadata } from "next";
+import "./globals.css";
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import {
+  arbitrum,
+  mainnet,
+  polygon,
+  moonbeam,
+  moonbaseAlpha,
+} from "wagmi/chains";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+const chains = [arbitrum, mainnet, polygon, moonbeam, moonbaseAlpha];
+
+const projectId = "34043931dedf67433e6f95bfa3205586"; //process.env.API_KEY as string
+
+const { publicClient } = configureChains(chains, [w3mProvider({ projectId })]);
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, chains }),
+  publicClient,
+});
+const ethereumClient = new EthereumClient(wagmiConfig, chains);
+
+
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
+    <WagmiConfig config={wagmiConfig}>
     <Provider store={store}>
+    <html lang="en">
+      <Providers />
       <body className={`${poppins.className}`}>{children}</body>
-    </Provider>
+      </html>
+    </Provider>=
+    </WagmiConfig>
   );
 }
+
