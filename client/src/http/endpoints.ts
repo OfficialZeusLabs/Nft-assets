b58e52e3d7643650b2dfeef015cf910ad1c33ed8
Hussein Mubarak <husseinmubarak500@gmail.com>
2023-09-21 19:20:02 +0100

    Changed live url

--- client/src/http/endpoints.ts
+++ client/src/http/endpoints.ts
@@ -1,7 +1,7 @@
 class Endpoints {
   static API_VERSION = "v1";
   static LOCAL_BASE_ROOT = "http://localhost:8000";
-  static REMOTE_BASE_ROOT = "https://nftfactory.onrender.com";
+  static REMOTE_BASE_ROOT = "https://nft-assets-ivory.vercel.app";
   static LOCAL_BASE = `${this.LOCAL_BASE_ROOT}/${this.API_VERSION}`;
   static REMOTE_BASE = `${this.REMOTE_BASE_ROOT}/${this.API_VERSION}`;
   static BASE_URL = this.REMOTE_BASE;
