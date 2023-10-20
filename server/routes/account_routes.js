5b568393af538b944980e5341a44f589b1e6181a
Hussein Mubarak <husseinmubarak500@gmail.com>
2023-10-20 11:37:26 +0100

    server: account ~ route

--- server/routes/account_routes.js
+++ server/routes/account_routes.js
@@ -2,6 +2,15 @@ const AccountRoutes = (apiVersion, servicePath = 'account') => {
     return {
         get REGISTER() {
             return `${apiVersion}/${servicePath}/register`
+        },
+        get FETCH_PROFILE() {
+            return `${apiVersion}/${servicePath}/profile`
+        },
+        get CREATE_PROFILE() {
+            return `${apiVersion}/${servicePath}/profile/create`
+        },
+        get UPDATE_PROFILE() {
+            return `${apiVersion}/${servicePath}/profile/update`
         }
     }
 }
