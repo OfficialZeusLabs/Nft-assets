82f16cd334aa1c222c29a93fcd1ff035a583762f
Hussein Mubarak <husseinmubarak500@gmail.com>
2023-10-20 16:27:24 +0100

    client: get wallet address when connected

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,22 @@
 "use client"; 
 
 import DashboardLayout from "@/layout/DashboardLayout";
+import { setWalletAddress } from "@/reducers/userSlice";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useAccount } from "wagmi";
 
 export default function Home() {
+  const dispatch = useDispatch();
+  const { address, isConnected } = useAccount();
+
+  useEffect(() => {
+    if(isConnected){
+      console.log(address);
+      dispatch(setWalletAddress(address));
+    }
+  }, []);
+  
   return (
     <>
       <DashboardLayout />
